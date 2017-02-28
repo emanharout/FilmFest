772b10b58ae0ee6034268c8a4d15606a3aa8a611
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-27 21:31:08 -0800

    Create DropShadow View

---
+++ FilmFest/DropShadowView.swift
@@ -0,0 +1,19 @@
+//
+//  DropShadowView.swift
+//  FilmFest
+//
+//  Created by Emmanuoel Haroutunian on 2/27/17.
+//  Copyright © 2017 Harrison Ferrone. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DropShadowView: UIView {
+
+  @IBInspectable var shadowOffset: CGSize = CGSize(width: 0, height: 0)
+  @IBInspectable var shadowOpacity: Float = 0
+
+  
+  
+}
