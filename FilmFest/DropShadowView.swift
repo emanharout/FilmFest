bca6e1e4f28410a0fd09bf71003dc7efdce3aa8e
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-27 21:39:31 -0800

    Setup DropShadow view to work with storyboards

--- FilmFest/DropShadowView.swift
+++ FilmFest/DropShadowView.swift
@@ -14,6 +14,28 @@ class DropShadowView: UIView {
   @IBInspectable var shadowOffset: CGSize = CGSize(width: 0, height: 0)
   @IBInspectable var shadowOpacity: Float = 0
 
+  // Following functions ensure UIView is redrawn when edited in storyboard
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    setup()
+  }
   
+  override func prepareForInterfaceBuilder() {
+    super.prepareForInterfaceBuilder()
+    
+    setup()
+  }
+  
+  override func draw(_ rect: CGRect) {
+    super.draw(rect)
+    
+    setup()
+  }
+  
+  func setup() {
+    layer.shadowOffset = shadowOffset
+    layer.shadowOpacity = shadowOpacity
+  }
   
 }
